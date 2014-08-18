27c3d6b2e5b7e48de71c8a48c344fe5257832585
Luke Carrier <luke@carrier.im>
2014-08-18 23:08:26 +0100

    Sidebar view is actually renderable
    Next step: fire off an event prior to render to allow modules to throw
    their items in. We should probably have a "sidebar:item-init" event or
    something...

--- app-web/script/lib/sidebar.js
+++ app-web/script/lib/sidebar.js
@@ -1,32 +1,71 @@
-var SidebarItems = Backbone.Collection.extend({
-    url: '/governor/sidebar_items'
-});
-
+/**
+ * Sidebar item view.
+ *
+ * Represents an individual sidebar item within the view hierarchy.
+ */
 var SidebarItemView = Backbone.View.extend({
     tagName: "li",
 
-    template: _.template('<a href=""></a>'),
+    template: _.template('<a href="<%= path %>"><%= label %></a>'),
 
     render: function() {
         this.$el.html(this.template(this.model));
         return this;
     }
-})
+});
 
+/**
+ * Sidebar item list view.
+ *
+ * Represents a list of sidebar items within the view hierarchy.
+ */
 var SidebarItemListView = Backbone.View.extend({
+    /**
+     * Parent element selector.
+     *
+     * @var string
+     */
     el: '.sidebar ul',
 
-    initialize: function() {
-        //
+    /**
+     * Array of sidebar items.
+     *
+     * @var SidebarItemView[]
+     */
+    items: [],
+
+    /**
+     * Add an item.
+     *
+     * @param SidebarItemView item The item to add.
+     *
+     * @return SidebarItemListView This instance, for method chaining.
+     */
+    addItem: function(item) {
+        this.items.push(item);
+        return this;
     },
 
+    /**
+     * Render the view.
+     *
+     * @return SidebarItemListView This instance, for method chaining.
+     */
     render: function() {
-        var sidebarItems = new SidebarItems();
-        sidebarItems.fetch({
-            success: function() {
-                this.$el.html('<li class="brand"><a href="#">Governor</a></li>');
-            }
-        });
+        var html  = '',
+            items = this.items.slice(0);
+
+        items.unshift(new SidebarItemView({
+            className: 'brand',
+            label:     'Governor',
+            path:      '#'
+        }));
+
+        childHtml = _.map(items, function(item) {
+            return item.render();
+        }).join('');
+
+        this.$el.html(childHtml);
 
         return this;
     }
